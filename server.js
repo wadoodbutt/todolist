5f3957000585690152ad4152afa23c8c04cf98c0
wadoodbutt <109189695+wadoodbutt@users.noreply.github.com>
2023-12-09 17:32:39 -0500

    More Initialization

--- server.js
+++ server.js
@@ -3,28 +3,29 @@ const bodyParser = require("body-parser");
 const app = express();
 const dotenv = require('dotenv').config({ path: './.env' });
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.kmw9jvg.mongodb.net/?retryWrites=true&w=majority`;
-const databaseAndCollection = {db: "CMSC335_DB", collection:"campApplicants"};
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.kmw9jvg.mongodb.net/?retryWrites=true&w=majority`;
+// const databaseAndCollection = {db: "CMSC335_DB", collection:"campApplicants"};
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
 
-async function run() {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-}
-run().catch(console.dir);
+// async function run() {
+//     await client.connect();
+//     await client.db("admin").command({ ping: 1 });
+// }
+// run().catch(console.dir);
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
 
 app.listen(3000);
 
@@ -32,5 +33,5 @@ app.get("/", (req, res) => {
     res.render("index")
 });
 
-const prompt = "Web server started and running at http://localhost:3000\nType stop to shutdown the server: ";
+const prompt = "Web server started and running at http://localhost:3000/";
 process.stdout.write(prompt);
